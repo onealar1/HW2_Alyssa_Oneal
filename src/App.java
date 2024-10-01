445d74e36e8f4f507e9e2189920c68da35a62440
onealar1 <onealar1@jacks.sfasu.edu>
2024-09-30 23:11:41 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import cs3331Helper.cs3331TicTacToeFrame;
+import cs3331Helper.cs3331TicTacToeGame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    public void start(Stage stage){
+
+        cs3331TicTacToeFrame frame = new cs3331TicTacToeFrame(stage);
+     
+        // Add the game here to the frame
+     
+        
+
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
+
